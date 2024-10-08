4b1b1ba300ecacf0086ac5598d959ef9332c9b30
thweepod <164258503+thweepod@users.noreply.github.com>
2024-10-08 14:04:58 +1100

    Add files via upload

---
+++ steam.nix
@@ -0,0 +1,20 @@
+# steam.nix
+
+{ pkgs, ... }:
+
+{
+
+ programs.steam = {
+   enable = true;
+   gamescopeSession.enable = true;
+   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+  ];
+
+  programs.gamemode.enable = true;
+
+}
